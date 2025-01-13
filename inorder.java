01091b8ac6c3aec6621cc77c6d1573b3b0410d0b
Priyansh-max <priyanshagarwal3381@gmail.com>
2025-01-13 17:43:59 +0530

    inorder_algo

---
+++ inorder.java
@@ -0,0 +1,27 @@
+public class inorder {
+    //inorder - Left Root Right
+    public void inorderTraversal(tree root){
+        if(root == null){
+            return;
+        }
+
+        inorderTraversal(root.left);
+        System.out.println(root.data);
+        inorderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        tree root = new tree(10);
+        root.right = new tree(9);
+        root.left = new tree(8);
+        root.right.right = new tree(7);
+        root.right.left = new tree(6);
+        root.left.left = new tree(5);
+        root.left.right = new tree(4);
+
+        inorder obj = new inorder();
+        //expected answer -- 5 8 4 10 6 9 7
+        obj.inorderTraversal(root);
+    }
+    
+}
