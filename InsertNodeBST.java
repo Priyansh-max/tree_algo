9ff67ac7cc34ff8f15323f4c6e276b3d622d2fb8
Priyansh-max <priyanshagarwal3381@gmail.com>
2025-02-19 18:14:49 +0530

    insert a node into a binary search tree

--- InsertNodeBST.java
+++ InsertNodeBST.java
@@ -1,41 +1,79 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InsertNodeBST {
 
     public tree helper(tree root , int insertNode){
-        while(root != null){
-            if(root.left == null && root.right == null){
-                if(root.data > insertNode){
-                    root.left = new tree(insertNode);
+        if(root == null){
+            return new tree(insertNode);
+        }
+
+        tree curr = root;
+        while(true){
+            if(insertNode < curr.data){
+                if(curr.left == null){
+                    curr.left = new tree(insertNode);
+                    break;
                 }
-                else{
-                    root.right = new tree(insertNode);
+                curr = curr.left;
+            }
+
+            else{
+                if(curr.right == null){
+                    curr.right = new tree(insertNode);
+                    break;
                 }
+                curr = curr.right;
             }
-            if(root.data > insertNode){
-                root = root.left;
-            }
-            if(root.data < insertNode){
-                root = root.right;
-            }
+
         }
 
         return root;
     }
 
+    public void printTree(tree root) {
+        if (root == null) {
+            System.out.println("Tree is empty");
+            return;
+        }
+
+        Queue<tree> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size(); // Number of nodes at the current level
+            for (int i = 0; i < levelSize; i++) {
+                tree node = queue.poll();
+                System.out.print(node.data + " ");
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            System.out.println(); // Move to next level
+        }
+    }
+
+    
+
     public static void main(String[] args) {
         tree root = new tree(10);
-        root.right = new tree(9);
-        root.left = new tree(8);
-        root.right.right = new tree(7);
-        root.right.left = new tree(6);
-        root.left.left = new tree(5);
-        root.left.right = new tree(4);
-        root.left.right.left = new tree(3);
+        root.right = new tree(13);
+        root.left = new tree(5);
+        root.right.right = new tree(14);
+        root.right.left = new tree(11);
+        root.left.left = new tree(3);
+        root.left.left.left = new tree(2);
+        root.left.left.right = new tree(4);
+        root.left.right = new tree(6);
+        root.left.right.right = new tree(9);
 
         InsertNodeBST obj = new InsertNodeBST();
 
-        obj.helper(root, 15);
+        tree newroot = obj.helper(root, 1);
 
-        
+        obj.printTree(newroot);
+
+
         
     }
     
